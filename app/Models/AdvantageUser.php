a3b14537f2b44e3c1eadffef6e81f1e587b1ba38
Dhiya <adiadiadha@google.com>
2022-09-14 15:04:20 +0700

    model & eloquent

---
+++ app/Models/AdvantageUser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class AdvantageUser extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+    
+    public $table = 'advantage_user';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'user_id', 
+        'advantage',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    // one to many
+    public function service()
+    {
+        return $this->belongsTo('App\Models\Service', 'service_id', 'id');
+    }
+
+}
---
+++ app/Models/Tagline.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Tagline extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+    
+    public $table = 'tagline';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'service_id', 
+        'tagline',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    // one to many
+    public function service()
+    {
+        return $this->belongsTo('App\Models\Service', 'service_id', 'id');
+    }
+
+}
